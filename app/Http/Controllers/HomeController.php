dd5e8de360cef6f2b4deda44c3adf30b850399b8
abdoforda <43405421+abdoforda@users.noreply.github.com>
2021-10-21 13:44:25 +0200

    Add User Model & Company Model

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware(['auth','verified']);
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        $user = User::find(1);
+        return $user;
+         auth()->user()->restore();
+         return "asdasd";
+        return view('home');
+    }
+}
